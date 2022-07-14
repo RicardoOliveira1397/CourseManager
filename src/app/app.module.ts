de18706dd6309e2ec8006015e9c75b5aab0601fa
unknown <rick.firmeoliveira2@gmail.com>
2022-07-13 21:30:21 -0300

    :recycle: [refactor] create delete method and coursemodule with own components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,29 @@
-import { CourseInfoComponent } from './courses/course-info.component';
-import { ReplacePipe } from './pipe/replace.pipe';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
+import { CourseModule } from './courses/course.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
 import { NavBarComponent } from './nav.bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent
   ],
   imports: [
+    CourseModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
